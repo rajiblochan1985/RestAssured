ed65094795e3114a859e2f4924e5076cb9b80fe2
rajiblochan1985 <rajiblochan.bdk1985@gmail.com>
2025-03-21 01:11:09 +0530

    Core Java

---
+++ selenium4project/src/test/java/CoreJavaPractice/PrintString.java
@@ -0,0 +1,12 @@
+package CoreJavaPractice;
+
+public class PrintString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Rajib Lochan Panigrahi");
+
+	}
+
+}
